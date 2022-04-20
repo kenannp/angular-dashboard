8b25554be4130baec6b4da850ae5f9ec9e8abbe7
kenan prtinac <kenan.prtinac@web.de>
2022-04-20 22:48:29 +0200

    Mobile view and Desktop view customized

--- src/app/dashboard/cards/card-one/card-one.component.ts
+++ src/app/dashboard/cards/card-one/card-one.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import ScreenState from "../../../../models/ScreenState";
-import {BreakpointObserver, Breakpoints, BreakpointState} from "@angular/cdk/layout";
-import {map} from "rxjs/operators";
+
 
 @Component({
   selector: 'app-card-one',
@@ -10,24 +8,6 @@ import {map} from "rxjs/operators";
 })
 export class CardOneComponent {
 
-  screenState = ScreenState.DESKTOP_TABLET;
-
-  screenStateObserver = this.breakpointObserver.observe(Breakpoints.Handset).subscribe((state: BreakpointState) => {
-      if (state.matches) {
-        this.screenState = ScreenState.MOBILE;
-      } else {
-        this.screenState = ScreenState.DESKTOP_TABLET;
-      }
-    })
-
-
-  getRowsByScreenState(): number{
-    if(this.screenState == ScreenState.DESKTOP_TABLET)
-      return 2
-    else
-      return 1
-  }
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  
 
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import ScreenState from "../../models/ScreenState";
+import {BreakpointObserver, Breakpoints, BreakpointState} from "@angular/cdk/layout";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  screenState = ScreenState.DESKTOP_TABLET;
+
+  screenStateObserver = this.breakpointObserver.observe(Breakpoints.Handset).subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.screenState = ScreenState.MOBILE;
+      } else {
+        this.screenState = ScreenState.DESKTOP_TABLET;
+      }
+    })
+
+
+  getRowsByScreenState(): number{
+    if(this.screenState == ScreenState.DESKTOP_TABLET)
+      return 1
+    else
+      return 2
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
